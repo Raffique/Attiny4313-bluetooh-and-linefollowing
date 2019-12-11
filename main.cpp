33015627d8e40cee8d8c6a9a5d70942a4d03a90c
Raffique <Raffique@users.noreply.github.com>
2019-12-11 08:39:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,11 +15,27 @@ ISR(USART_RXC_vect){
 int main(void){
 
     DDRB = 0x00;
-    DDRD = (1<<PD5) | (1<<PD4)
-    DDRD &= ~(1<<PD3)
+    DDRD = (1<<PD5) | (1<<PD4);
+    DDRD &= ~(1<<PD3);
     
-    TCCR0 = ()
+    TCCR0 = (1<<WGM00) | (1<<WGM01) | (1<<COM01) | (1<<CS01);
+    TCNT0 = 0;
+    OCR0 = 0;
+    
+    UCSRB |= (1<<RXEN);
+    UCSRC = (1<<URSEL) | (1<<USBS) | (3<<UCSZ0);
+    UCSRC = (1<<UPM1) | (1<<UPM0);
+    UBBRL = MYUBRR;
+    UBRRM = (MYUBRR >> 8);
+    UCSRB |= (1<<RXCIE);
     
     sei();
+    
+    while(1){
+    
+    }
+    
+    return 0;
+    
 
 }
