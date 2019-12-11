12b232af8ec46e03ac25eaaa6730cb5a567344c7
Raffique <Raffique@users.noreply.github.com>
2019-12-11 09:18:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,6 +29,19 @@ int main(void){
     UBRRM = (MYUBRR >> 8);
     UCSRB |= (1<<RXCIE);
     
+    // OC1A, OC1B outputs
+	DDRB |= (1<<PB4)|(1<<PB3);
+	// TOP, set for 50Hz (20ms)
+	ICR1 = 20000;
+	// Center outputs (1.5ms)
+	OCR1A = 1500;
+	OCR1B = 1500;
+	// Timer 1 fast PWM mode 14 
+	// Clear on compare, set at TOP
+	// /8 prescaler
+	TCCR1A = (1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);
+	TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11);
+    
     sei();
     
     while(1){
@@ -44,28 +57,65 @@ void steering(byte direction){
     
     switch(direction){
             
-        case 0b00000000:
+        case 0b10000001:
+		case 0b11000011:
+        case 0b11100111:
+            
+			break;
+			
+		case 0b10000111:
+			break;
+            
+		case 0b00001111:
+			break;
+			
+		case 0b11000111:
+			break;
+            
+		case 0b10000011:
+			break;
+			
+		case 0b11100001:
             break;
             
-        case 0b00000000:
-            break;
+		case 0b11110000:
+			break;
+			
+		case 0b11100011:
+			break;
             
-        case 0b00000000:
-            break;
+		case 0b11000001:
+			break;
+			
+		case 0b00111111:
+		case 0b01111111:
+			break;
             
-        case 0b00000000:
-            break;
+		case 0b11111100:
+		case 0b11111110:
+			break;
             
-        case 0b00000000:
-            break;
+		case 0b11111111:
+			break;
+			
+		case 0b00000000:
+			break;
             
-        case 0b00000000:
-            break;
+		case 0b00000111:
+			break;
+		
+		case 0b00000011:
+		case 0b00000001:
+			break;
             
-        case 0b00000000:
-            break;
+		case 0b11100000:
+			break;
             
-        case 0b00000000:
+		case 0b11000000:
+		case 0b10000000:
+			break;
+            
+		default:
             break;
     
             
